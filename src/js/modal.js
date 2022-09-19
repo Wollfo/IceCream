ed9a3771a2f2dbf11c66b04adcf0e07b8227e5c7
Stanislav Kushnir <96527948+St-Kushnir@users.noreply.github.com>
2022-09-20 01:09:43 +0300

    модалка секції about

--- src/js/modal.js
+++ src/js/modal.js
@@ -57,3 +57,18 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-about]"),
+      closeModalBtn: document.querySelector("[data-modal-close-about]"),
+      modal: document.querySelector("[data-modal-about]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
