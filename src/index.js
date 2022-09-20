1d407fbca4f2037a285967a2bb2f23764fe8b3eb
Stanislav Kushnir <96527948+St-Kushnir@users.noreply.github.com>
2022-09-20 13:40:01 +0300

    розібрався ззакриванням модалок

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './js/menu';
-import './js/modal';
+import './js/modal-header';
+import './js/modal-mobile-menu';
+import './js/modal-products';
+import './js/modal-about';
+import './js/modal-our-locations';
+import './js/modal-franchise';
 import './js/slider';
--- src/js/modal-products.js
+++ src/js/modal-products.js
@@ -1,35 +1,5 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
-
-  (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open-mobile-menu]"),
-      closeModalBtn: document.querySelector("[data-modal-close-mobile-menu]"),
-      modal: document.querySelector("[data-modal-mobile-menu]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
-
-  (() => {
-    const refs = {
       openModalBtn: document.querySelector("[data-modal-open-products-1]"),
       closeModalBtn: document.querySelector("[data-modal-close-products-1]"),
       modal: document.querySelector("[data-modal-products-1]"),
@@ -63,21 +33,6 @@
       openModalBtn: document.querySelector("[data-modal-open-products-3]"),
       closeModalBtn: document.querySelector("[data-modal-close-products-3]"),
       modal: document.querySelector("[data-modal-products-3]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
-
-  (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open-about]"),
-      closeModalBtn: document.querySelector("[data-modal-close-about]"),
-      modal: document.querySelector("[data-modal-about]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
